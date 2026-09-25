74e36dbb3fb53a465be339bbcd814d724e801c79
SILA Network <pssd500@gmail.com>
2026-09-25 21:44:16 +0300

    sync: import Sila KZG elliptic helpers part 2
    Upstream-c-kzg: 57963c66122b4ae78db4aa9b908eff7262d8a51e
    
    Signed-off-by: SILA Network <pssd500@gmail.com>

--- src/sip7594/poly.c
+++ src/sip7594/poly.c
@@ -19,8 +19,8 @@
 #include "common/ec.h"
 #include "common/ret.h"
 #include "common/utils.h"
-#include "setup/settings.h"
 #include "sip7594/fft.h"
+#include "setup/settings.h"
 
 #include <stdlib.h> /* For NULL */
 #include <string.h> /* For memcpy */
@@ -38,8 +38,8 @@
 void shift_poly(fr_t *p, size_t len, const fr_t *shift_factor) {
     fr_t factor_power = FR_ONE;
     for (size_t i = 1; i < len; i++) {
-        blst_fr_mul(&factor_power, &factor_power, shift_factor);
-        blst_fr_mul(&p[i], &p[i], &factor_power);
+        fr_mul(&factor_power, &factor_power, shift_factor);
+        fr_mul(&p[i], &p[i], &factor_power);
     }
 }
 
